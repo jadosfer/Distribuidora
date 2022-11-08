cd0075134dfbecef3f4268cc1e749c9c34276049
javierfm <jadosfer@hotmail.com>
2022-11-08 12:02:47 -0300

    a

--- src/app/pruebas/prueba.component.ts
+++ src/app/pruebas/prueba.component.ts
@@ -1,32 +1,32 @@
-// import { Component, OnInit } from '@angular/core';
-// import { Observable } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
-// @Component({
-//   selector: 'app-prueba',
-//   templateUrl: './prueba.component.html',
-//   styleUrls: ['./prueba.component.scss']
-// })
-// export class PruebaComponent implements OnInit {
-//   count$: Observable<number>
+@Component({
+  selector: 'app-prueba',
+  templateUrl: './prueba.component.html',
+  styleUrls: ['./prueba.component.scss']
+})
+export class PruebaComponent implements OnInit {
+  count$: Observable<number>
 
-//   constructor() {
-//     // TODO: Connect `this.count$` stream to the current store `count` state
-//   }
+  constructor() {
+    // TODO: Connect `this.count$` stream to the current store `count` state
+  }
 
-//   ngOnInit(): void {
-//   }
+  ngOnInit(): void {
+  }
 
-//   increment() {
-//     // TODO: Dispatch an increment action
-//   }
+  increment() {
+    // TODO: Dispatch an increment action
+  }
 
-//   decrement() {
-//     // TODO: Dispatch a decrement action
-//   }
+  decrement() {
+    // TODO: Dispatch a decrement action
+  }
 
-//   reset() {
-//     // TODO: Dispatch a reset action
-//   }
+  reset() {
+    // TODO: Dispatch a reset action
+  }
 
 
-// }
+}
