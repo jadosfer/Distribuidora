2822a9a34c4444bd44df39a8fe8a7a5ffe81373a
jadosfer <jadosfer@hotmail.com>
2023-06-05 12:55:19 -0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,1 @@
-export const environment = {
-  production: true,
-  firebase: {
-    apiKey: "AIzaSyDQFsWCl9FGxEr0BB6f-Bhp9h3eRQsCbHY",
-    authDomain: "distribuidora-nb.firebaseapp.com",
-    //authDomain: "http://localhost:9099",
-    databaseURL: "http://127.0.0.1:9000",
-    projectId: "distribuidora-nb",
-    storageBucket: "distribuidora-nb.appspot.com",
-    messagingSenderId: "763137673585",
-    appId: "1:763137673585:web:f9194415b214fddd014ed0"
-  }
-};
+
