412f9313c045d9a4d1e38266641cedc7b54e9a8b
javierfm <jadosfer@hotmail.com>
2023-06-05 14:57:23 -0300

    recupero environmets

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,1 +1,12 @@
-
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBzHzW1kMlgLbPUM3cYy1B9UZRHbV98IIE",
+    authDomain: "distrib-42bdb.firebaseapp.com",
+    databaseURL: "https://distrib-42bdb-default-rtdb.firebaseio.com",
+    projectId: "distrib-42bdb",
+    storageBucket: "distrib-42bdb.appspot.com",
+    messagingSenderId: "1032461943463",
+    appId: "1:1032461943463:web:ea663acb33dfc7be1f9182"
+  }
+};
